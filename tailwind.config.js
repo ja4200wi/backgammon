184f786461b46f787fe212f3682c0759befeea8e
Jann Winter <jannwinter@icloud.com>
2024-09-06 13:33:43 +0200

    add our color const to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './App.tsx'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#302E2B',
+        backgroundTransparent: 'rgba(84, 80, 75, 0.9)',
+        headerBackGroundColor: '#312F2C',
+        cardBackgroundColor: '#54504B',
+        iconGrey: '#B5B6B5',
+        darkGrey: '#262522',
+        standardGrey: '#C7C7C7',
+        buttonBlueFontColor: '#007AFF',
+        buttonDisabledFontColor: '#808080',
+        appGreen: '#6B9C41',
+        appYellow: '#F4C300',
+        appBlue: '#597FD1',
+        appRed: '#C24542',
+
+        primaryColor: '#283618',
+        secondaryColor: '#fefae0',
+        tertiaryColor: '#bc6c25',
+      },
+    },
   },
   plugins: [],
 };
