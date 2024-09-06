6de7389d890c8726f754a70fd52a1776f7a2e299
Jann Winter <jannwinter@icloud.com>
2024-09-06 09:48:07 +0200

    Add user joined data

--- service/profileService.tsx
+++ service/profileService.tsx
@@ -9,3 +9,8 @@ export async function getUserNickname(): Promise<string> {
   const { nickname } = await fetchUserAttributes();
   return nickname || getUserName();
 }
+
+export async function getUserJoined(): Promise<string> {
+  const userAttributes = await fetchUserAttributes();
+  return userAttributes.updated_at || 'Unknown'; //TODO: Change to created_at
+}
