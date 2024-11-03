06503f5b16cf179f45ccc982badc0acc490d4382
Jann Winter <jannwinter@icloud.com>
2024-11-03 12:26:36 +0100

    install eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['app/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
